8ec6e7bc34527ad7fdef89851f42038d65520ffa
farasali12 <79818408+farasali12@users.noreply.github.com>
2023-03-08 13:34:44 -0800

    Create faras.java

---
+++ src/midtermexam_versiona_extensioncode/faras.java
@@ -0,0 +1,17 @@
+
+/**
+ * This is a simple Java class that prints "Hello, World!" to the console.
+ * It is used as an example for learning how to write Java programs.
+ */
+public class HelloWorld {
+    
+    /**
+     * The main method is the entry point of the program.
+     * It prints the message "Hello, World!" to the console.
+     * @param args an array of strings representing command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
